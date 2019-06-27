0b05550259d707dcf3b08bfa66f707993d0cd659
Berezhnyi <kivin13@gmail.com>
2019-06-27 16:10:07 +0300

    added function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import $ from 'jquery';
 
-function gotCompassReading (reading) {
-	$('.value').text(reading.magneticHeading);
+let scaler = document.querySelector('.scale');
+let degreeValue = document.querySelector('.scale');
+
+// if (window.DeviceOrientationEvent) {
+// 	window.addEventListener('deviceorientation', function (event) {
+// 		// alpha: rotation around z-axis
+// 		let rotateDegrees = event.alpha;
+
+// 		// gamma: left to right
+// 		// let leftToRight = event.gamma;
+
+// 		// beta: front back motion
+// 		// let frontToBack = event.beta;
+
+// 		handleOrientationEvent(rotateDegrees);
+// 	}, true);
+// }
+
+// let handleOrientationEvent = function (rotateDegrees) {
+// 	degreeValue.innerHTML = rotateDegrees;
+// };
+
+function handleOrientation (event) {
+	let degrees = event.alpha;
+
+	degreeValue.innerHTML = degrees;
 }
+window.addEventListener('deviceorientation', handleOrientation);
